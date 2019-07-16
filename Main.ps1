b3a284fcc285b7e5129321e257ba6517c1ca10d1
Rogerio-sys <rogeriolageirareis@gmail.com>
2019-07-16 16:25:06 +0200

    Main et Les 3 fonctions
    Fonctions:
    Add
    Crea
    Random

---
+++ Main.ps1
@@ -0,0 +1,5 @@
+#Main
+
+Import-module $env:USERPROFILE\Documents\GitHub\Projet_Powershell\Fonctions\AddOUforUserX
+Import-module $env:USERPROFILE\Documents\GitHub\Projet_Powershell\Fonctions\CreaUsers
+Import-module $env:USERPROFILE\Documents\GitHub\Projet_Powershell\Fonctions\random-password
